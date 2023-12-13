ee75846703e3010f6ba1b4e44369ef2765f4cfd2
brunavazmattos <brunavazmattos@gmail.com>
2023-12-13 08:28:23 +0000

    this is the presentation commit, nothing added to it

--- app/src/main/java/com/example/mobiledevolopment/MyApp.kt
+++ app/src/main/java/com/example/mobiledevolopment/MyApp.kt
@@ -1,7 +1,7 @@
 package com.example.mobiledevolopment
 import android.app.Application
 import com.google.firebase.FirebaseApp
-
+//this is the code of presentation
 class MyApp: Application() {
     override fun onCreate() {
         super.onCreate()
